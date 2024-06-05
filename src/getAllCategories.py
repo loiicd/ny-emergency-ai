0dc0af58f8b37ab4ff3a4d64c57a10a4a04e01ca
Doerr, Loic <loic.doerr@haufe-akademie.de>
2024-06-05 14:12:59 +0200

    changes

--- src/getAllCategories.py
+++ src/getAllCategories.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 df = pd.read_json('/Users/loic.doerr/dev/ny-emergency-ai/data/fire-incidents/interims.json')
 
-categories = df['incident_classification_group'].unique()
+categories = df[['incident_classification', 'incident_classification_group']]
 
-print(categories)
+unique_categories = categories.drop_duplicates()
+
+category_list = unique_categories.to_dict(orient='records')
+
+print(category_list)
